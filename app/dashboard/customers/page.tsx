8c5e430a8962f6c1623bc46fe247c69bb036b2f2
sagarraCM <sagar.rajshekar@citymall.live>
2024-05-26 22:38:29 +0530

    My Next js Project

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function Page (){
+    return(
+        <p>Hi i am Customers page</p>
+    )
+}
---
+++ app/dashboard/vendors/page.tsx
@@ -0,0 +1,3 @@
+export default function Page (){
+    return <p>Hii i am Vendor page </p>
+}
